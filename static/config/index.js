e3455eec835f9b6429c7a2f2f6fb2bb736de7547
ZhangHao <605901202@qq.com>
2019-10-15 20:43:35 +0800

    完善地区级联选择框信息

--- static/config/index.js
+++ static/config/index.js
@@ -2,14 +2,14 @@
  * 开发环境
  */
 ;(function () {
-  window.SITE_CONFIG = {};
+  window.SITE_CONFIG = {}
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://39.100.242.26:80/CompanyInformation';
+  window.SITE_CONFIG['baseUrl'] = 'https://company.chefile.com/CompanyInformation'
   // window.SITE_CONFIG['baseUrl'] = 'http://127.0.0.1:80/CompanyInformation';
 
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['domain']  = './'; // 域名
-  window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
-  window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
-})();
+  window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
+  window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
+})()
